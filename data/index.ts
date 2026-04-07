c3bba785b1b137814a0de9decefe4527a6f44f19
JonsAZ <jonstomassoyan1104@gmail.com>
2026-04-07 15:50:00 +0800

    fix

--- data/index.ts
+++ data/index.ts
@@ -128,21 +128,18 @@ export const testimonials = [
       "Collaborating with Jones was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jones's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jones is the ideal partner.",
     name: "Michael",
     title: "Director of Huabao",
-    img: "/portfolio/profile.svg",
   },
   {
     quote:
       "Collaborating with Jones was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jones's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jones is the ideal partner.",
     name: "Michael",
     title: "Director of Huabao",
-    img: "/portfolio/profile.svg",
   },
   {
     quote:
       "Collaborating with Jones was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jones's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jones is the ideal partner.",
     name: "Michael",
     title: "Director of Huabao",
-    img: "/portfolio/profile.svg",
   },
 ];
 
